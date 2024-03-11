c7cf5bfb009b6495978d7dbc45caceb9e75940a0
lokesh143b <srirangamlokeswararao@gmail.com>
2024-03-11 20:43:43 +0530

    first commit

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -3,7 +3,7 @@ import { IoIosArrowForward } from "react-icons/io";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+  
 const data = [
   {
     id: 1,
